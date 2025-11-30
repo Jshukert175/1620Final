22d2c7f66e9ba0f1779217cc1d94c57da7751d27
Jshukert175 <jess.shukert@gmail.com>
2025-11-30 13:23:11 -0600

    Updated logic/gui

--- logic.py
+++ logic.py
@@ -7,3 +7,31 @@ class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.button_submit.clicked.connect(lambda : self.submit())
+
+    def submit(self):
+        vote_id = int(self.input_id.text())
+        try:
+            if len(vote_id) < 4:
+                raise ValueError
+        except ValueError:
+            self.label_name.setText(text="Invalid ID")
+            return
+        if self.jane.isChecked():
+            vote = "Jane"
+        elif self.john.isChecked():
+            vote = "John"
+        else:
+            vote = "Vote for a candidate"
+
+        if self.votes.checkedButton() is not None:
+            self.votes.setExclusive(False)
+            self.votes.checkedButton().setChecked(False)
+            self.votes.setExclusive(True)
+
+        with open('data.csv', 'a', newline='') as csvfile:
+            content = csv.writer(csvfile)
+            content.writerow([vote_id, vote])
+
+        self.vote_id.clear()
